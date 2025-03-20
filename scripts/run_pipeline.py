4a0195273ba18171cb605177d66122541f9d1d0a
giorgia-l <lini.giorgiale@gmail.com>
2025-03-20 09:16:11 +0100

    delete comment

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -4,11 +4,13 @@ sys.path.append(os.path.abspath('..'))  # Adds the parent directory to sys.path
 
 import logging
 from src import config
-from src.load_data import load_data
-from src.preprocess import preprocess_data
+from src.load_data import load_data #sto richiamando un altro codice python, apriamolo wow:
+from src.preprocess import preprocess_data #chiama pure preprocess
 from src.make_model import train_model
 # from src.evaluation import evaluate_model
 # from src.save_results import save_predictions
+# Perchè abbiamo messo il pipelyne che richiama altri codici? per evitare di mettere troppo codice e
+# farlo girare. divido a livello logico le cose che fanno diverse cose 
 
 # Set up logging
 logging.basicConfig(filename='../log/pipeline.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -27,20 +29,6 @@ def main():
     # Step 3: Train sentiment analysis model
     logging.info("Training the model...")
     train_model()
-
-    # # Step 4: Evaluate model performance
-    # logging.info("Evaluating the model...")
-    # evaluate_model(model, vectorizer, config.DATABASE_PATH)
-
-    # # Step 5: Predict sentiment on new/processed data
-    # logging.info("Making predictions...")
-    # predictions = predict_sentiment(model, vectorizer, config.DATABASE_PATH)
-
-    # # Step 6: Save results to the database
-    # logging.info("Saving predictions to database...")
-    # save_predictions(predictions, config.DATABASE_PATH)
-
-    # logging.info("Sentiment Analysis Pipeline completed successfully!")
 
 if __name__ == "__main__":
     main()
